f7a52538038c39cd2685b1cfc50f8f697a01357e
王嘉越（002280） <wangjiayue@fcbox.com>
2021-04-07 10:42:07 +0800

    ''update

--- src/main/java/com/gawa/algorithm/linear/LinkList.java
+++ src/main/java/com/gawa/algorithm/linear/LinkList.java
@@ -1,6 +1,8 @@
 package com.gawa.algorithm.linear;
 
-public class LinkList<T> {
+import java.util.Iterator;
+
+public class LinkList<T> implements Iterable<T>{
     //记录头结点
     private Node head;
     //记录链表的长度
@@ -38,7 +40,7 @@ public class LinkList<T> {
     }
 
     //判断链表是否为空
-    public boolean isEnpty() {
+    public boolean isEmpty() {
         return N==0;
     }
 
@@ -51,6 +53,25 @@ public class LinkList<T> {
         }
         return n.item;
     }
+
+    //向链表中添加元素t
+    public void insert(T t) {
+        //找到当前最后一个结点
+
+        Node n = head;
+        while(n.next!=null){
+            n=n.next;
+        }
+
+
+        //创建新结点，保存元素t
+        Node newNode = new Node(t, null);
+        //让当前最后一个结点指向新结点
+        n.next=newNode;
+        //元素的个数+1
+        N++;
+    }
+
 
     //向指定位置i处，添加元素t
     public void insert(int i, T t) {
@@ -77,4 +98,55 @@ public class LinkList<T> {
     public int iondexof(T t) {
         return -1;
     }
+
+    //用来反转整个链表
+    public void reverse(){
+
+        //判断当前链表是否为空链表，如果是空链表，则结束运行，如果不是，则调用重载的reverse方法完成反转
+        if (isEmpty()){
+            return;
+        }
+
+        reverse(head.next);
+    }
+
+    //反转指定的结点curr，并把反转后的结点返回
+    public Node reverse(Node curr){
+        if (curr.next==null){
+            head.next=curr;
+            return curr;
+        }
+        //递归的反转当前结点curr的下一个结点；返回值就是链表反转后，当前结点的上一个结点
+        Node pre = reverse(curr.next);
+        //让返回的结点的下一个结点变为当前结点curr；
+        pre.next=curr;
+        //把当前结点的下一个结点变为null
+        curr.next=null;
+        return curr;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new LIterator();
+    }
+
+    private class LIterator implements Iterator{
+        private Node n;
+        public LIterator(){
+            this.n=head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return n.next!=null;
+        }
+
+        @Override
+        public Object next() {
+            n = n.next;
+            return n.item;
+        }
+    }
+
+
 }
